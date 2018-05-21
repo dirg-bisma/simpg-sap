d6cd76a78b97a322cd0029ff8dac80e13099ce72
hendrik subangkit <hendrik.hari.s@gmail.com>
2018-05-21 16:09:15 +0700

    add grand total laporan angkutan

--- application/config/database.php
+++ application/config/database.php
@@ -50,10 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-// $db['default']['password'] = '123456';
-// $db['default']['database'] = 'simpg_ptpn';
-$db['default']['password'] = '';
-$db['default']['database'] = 'simpg';
+$db['default']['password'] = '123456';
+$db['default']['database'] = 'simpg_ptpn';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
