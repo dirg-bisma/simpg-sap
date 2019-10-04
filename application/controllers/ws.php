2fa895c3142bf6be53a030c3f1ac0639ee3364fb
kruakz <ferdy.kruak@gmail.com>
2019-10-04 09:20:19 +0700

    WS curl

---
+++ application/controllers/ws.php
@@ -0,0 +1,14 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Ws extends SB_Controller
+{
+	function __construct() {
+        parent::__construct();
+        
+    }
+
+    function masterfield(){
+    	$a = $this->db->query("SELECT * FROM sap_field")->result();
+    	return json_encode($a,true);
+    }
+}
